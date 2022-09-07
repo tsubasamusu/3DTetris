df089934e9395a0584c2b513279ec867d958f28f
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-07 12:57:23 +0900

    蓄積されたブロックが横一列に揃ったかどうかを検知する処理を完成させた。

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -9,7 +9,7 @@ public class BlockManager : MonoBehaviour
     [SerializeField]
     private BlockGenerator blockGenerator;//BlockGenerator
 
-    [HideInInspector]
+    //[HideInInspector]
     public List<GameObject> cubeList=new List<GameObject>();//現在、ステージ上に蓄積されている立方体のリスト
 
     private GameObject currentBlock;//現在アクティブなブロック
@@ -41,11 +41,14 @@ public class BlockManager : MonoBehaviour
     /// </summary>
     public void StoppedCurrentBlock()
     {
-        //現在アクティブなブロックの孫の数だけ繰り返す
-        for(int i = 0; i < currentBlock.transform.GetChild(0).transform.childCount; i++)
+        //4回繰り返す
+        for(int i = 0; i < 4; i++)
         {
-            //現在アクティブなブロックの全ての孫をリストに追加
-            cubeList.Add(currentBlock.transform.GetChild(0).transform.GetChild(i).gameObject);
+            //現在アクティブなブロックの孫0号をリストに追加
+            cubeList.Add(currentBlock.transform.GetChild(0).transform.GetChild(0).gameObject);
+
+            //現在アクティブなブロックの孫0号の親を自身に設定
+            currentBlock.transform.GetChild(0).transform.GetChild(0).transform.SetParent(transform);
         }
 
         //現在アクティブなブロックからBlockControllerを取得出来たら
@@ -61,9 +64,40 @@ public class BlockManager : MonoBehaviour
             Debug.Log("現在アクティブなブロックからのBlockControllerの取得に失敗");
         }
 
-        //TODO:ブロックの消化の確認
+        //立方体の消化を行うか確認する
+        CheckDigested();
 
         //ブロックを1度生成し、生成したブロックの情報を取得
         currentBlock = blockGenerator.GenerateBlock();
+    }
+
+    /// <summary>
+    /// 立方体の消化を行うかどうか確認し、場合に応じて消化を行う
+    /// </summary>
+    private void CheckDigested()
+    {
+        //20回繰り返す
+        for(int i = 1; i < 21; i++)
+        {
+            //同じy座標の立方体のリストを作成
+            List<GameObject> samePosYList = cubeList.FindAll(x => (x.transform.position.y > (i - 0.5f)) && (x.transform.position.y < (i + 0.5f)));
+            
+            //同じy座標の立方体の数が10より小さかったら（横一列が揃っていなかったら）
+            if(samePosYList.Count< 10)
+            {
+                //次の繰り返し処理へ移る
+                continue;
+            }
+
+            //10回繰り返す
+            for(int j = 0; j < 10; j++)
+            {
+                //対象の立方体を一定時間後に消す
+                Destroy(cubeList.FindAll(x => (x.transform.position.y > (i - 0.5f)) && (x.transform.position.y < (i + 0.5f)))[j],0.25f);
+
+                //対象の立方体をリストから取り除く
+                cubeList.Remove(cubeList.Find(x => (x.transform.position.y > (i - 0.5f)) && (x.transform.position.y < (i + 0.5f))));
+            }
+        }
     }
 }

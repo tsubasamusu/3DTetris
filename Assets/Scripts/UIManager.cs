94e826cdb8cfd13594f44fc66099a1cf0240e3e2
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-10 10:26:04 +0900

    リスタートボタンを押したときに、効果音が2重で再生されるバグを修正。

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -135,6 +135,9 @@ public class UIManager : MonoBehaviour
         //ボタンが押されるまで待つ
         yield return new WaitUntil(() => clicked == true);
 
+        //効果音を再生
+        SoundManager.instance.PlaySound(SoundDataSO.SoundName.BtnGameStartSE);
+
         //マウスカーソルを非表示にする
         Cursor.visible = false;
 
@@ -164,9 +167,6 @@ public class UIManager : MonoBehaviour
         {
             //ボタンが押された状態に切り替える
             clicked = true;
-
-            //効果音を再生
-            SoundManager.instance.PlaySound(SoundDataSO.SoundName.BtnGameStartSE);
 
             //ボタンを非活性化
             button.interactable = false;
@@ -227,6 +227,9 @@ public class UIManager : MonoBehaviour
         //ボタンが押されるまで待つ
         yield return new WaitUntil(() => clicked == true);
 
+        //効果音を再生
+        SoundManager.instance.PlaySound(SoundDataSO.SoundName.BtnRestartSE);
+
         //背景を白色に変更
         imgBackGround.DOColor(Color.white, 1f);
 
@@ -250,9 +253,6 @@ public class UIManager : MonoBehaviour
         {
             //ボタンが押された状態に切り替える
             clicked = true;
-
-            //効果音を再生
-            SoundManager.instance.PlaySound(SoundDataSO.SoundName.BtnRestartSE);
 
             //ボタンを非活性化
             button.interactable = false;
@@ -313,6 +313,9 @@ public class UIManager : MonoBehaviour
         //ボタンが押されるまで待つ
         yield return new WaitUntil(() => clicked == true);
 
+        //効果音を再生
+        SoundManager.instance.PlaySound(SoundDataSO.SoundName.BtnRestartSE);
+
         //ロゴを非表示にする
         imgLogo.DOFade(0f, 1f);
 
@@ -336,9 +339,6 @@ public class UIManager : MonoBehaviour
         {
             //ボタンが押された状態に切り替える
             clicked = true;
-
-            //効果音を再生
-            SoundManager.instance.PlaySound(SoundDataSO.SoundName.BtnRestartSE);
 
             //ボタンを非活性化
             button.interactable = false;
